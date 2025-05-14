dfc6a786c8e9a83a40734d5f95e631baf50e3279
Maíra Paula <mairapaulac@hotmail.com>
2025-05-13 21:22:23 -0300

    adding new functions

--- linked-lists/singly_linked_list.c
+++ linked-lists/singly_linked_list.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdbool.h>
 
 typedef struct node {
     int data;
@@ -7,16 +8,12 @@ typedef struct node {
 } node;
 
 //criando a cabeça da lista
-void create_head(node **head) {
-    *head = (node*)malloc(sizeof(node));
-    if (*head != NULL) {
-        (*head)->next = NULL;
-    } else {
-        printf("Erro ao alocar memoria\n");
-    }
+void cria_cabeca(node **head) {
+    *head = NULL;
 };
 
-void print_list(node *head) {
+//mostrar os elementos da lista
+void printa_lista(node *head) {
     node *temp = head;
 
     while (temp != NULL) {
@@ -25,10 +22,22 @@ void print_list(node *head) {
     }
 }
 
+//retorna o tamanho da lista
+int tamanho_lista(node **head) {
+    int cont = 0;
+    node *temp = *head;
+
+    while (temp != NULL){
+        temp = temp->next;
+        cont++;
+    }
+
+    return cont;            
+}
+
 //inserindo ao final
-void insert_end(node **head, int value) {
+void insere_final(node **head, int value) {
     node *new_node = (node*)malloc(sizeof(node)); 
-    
 
     // verificando se conseguimos alocar
     if(new_node == NULL)
@@ -49,25 +58,53 @@ void insert_end(node **head, int value) {
     }
 }
 
-//inserir no começo da lista
-void insert(node **head, int value) { 
+//inserir em uma dada posição
+void insere_em_k_pos(node **head, int value, int pos) { 
     node *new_node = (node*)malloc(sizeof(node));
 
     if(new_node == NULL)
         printf("Erro ao alocar memoria\n");
+
+    if(pos == 0 || pos > tamanho_lista(head))
+        printf("Erro, posicao invalida");
 
    new_node->data = value;
    new_node->next = NULL;
 
    if(*head == NULL) {
         *head = new_node;
-   } else {
-       
-        new_node->next = *head;
-        *head = new_node;
+   
    }
 
+    if(pos == 1) {
+        new_node->next = *head;
+        *head = new_node;
+    } else {
+        node *temp = *head;
+            for (int i = 0; i < pos - 1 && temp != NULL; i++) {
+                    temp = temp->next;
+            }
+            new_node->next = temp->next;
+            temp->next = new_node;
+        }
+    }      
+
+
+bool procura_elemento(node **head, int key) {
+    node *temp = *head;
+
+    while(temp != NULL) {
+        if (temp->data)
+            return true;
+            
+            
+        temp = temp->next;
+    }
+    return false;
 }
+
+
+
 
 
 
@@ -81,10 +118,12 @@ int main (void) {
     insert_end(&list, 3);
     insert_end(&list, 4);
     insert_end(&list, 5);
-    insert(&list, 10);
-    insert(&list, 9);
-    insert(&list, 8);
-    insert(&list, 7);
+    insert_end(&list, 10);
+    insert_end(&list, 9);
+    insert_end(&list, 8);
+    insert_end(&list, 7);
+
+    printf("Tamanho: %d\n", tamanho_lista(&list));
 
     print_list(list);
 }
